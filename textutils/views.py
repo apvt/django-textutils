53981734116c75deb0369280bcaa3b81e502054a
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2023-02-15 00:37:59 +0530

    Add files via upload

---
+++ textutils/views.py
@@ -0,0 +1,105 @@
+
+# Views.py
+# I have created this file - Harry
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+    # return HttpResponse("Home")
+
+#def about(request):
+    # return HttpResponse("about pg")
+
+
+def ex1(request):
+    sites = ['''<h1>For Entertainment  </h1> <a href="https://www.youtube.com/"> Youtube Videos</a> ''',
+             '''<h1>For Interaction  </h1> <a href="https://www.facebook.com/"> Facebook</a> ''',
+             '''<h1>For Insight  </h1> <a href="https://www.ted.com/talks"> Ted Talks</a> ''',
+             '''<h1>For Internship  </h1> <a href="https://www.internshala.com">Internship</a> ''']
+    return HttpResponse((sites))
+
+def analyze(request):
+    #Get the text
+    djtext = request.POST.get('text', 'default')
+
+
+    # Check checkbox values
+    removepunc = request.POST.get('removepunc', 'off')
+    uppercase = request.POST.get('uppercase', 'off')
+    lowercase = request.POST.get('lowercase', 'off')
+    newlineremover = request.POST.get('newlineremover', 'off')
+    extraspaceremover = request.POST.get('extraspaceremover', 'off')
+
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+
+        params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
+        djtext = analyzed
+        # return render(request, 'analyze.html', params)
+
+    if(uppercase=="on"):
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+
+        params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
+        djtext = analyzed
+        # Analyze the text
+        # return render(request, 'analyze.html', params)
+        
+    if(lowercase=="on"):
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.lower()
+
+        params = {'purpose': 'Changed to Lowercase', 'analyzed_text': analyzed}
+        djtext = analyzed
+        # Analyze the text
+        # return render(request, 'analyze.html', params)     
+
+    if(extraspaceremover=="on"):
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index+1]==" "):
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        djtext = analyzed
+        # Analyze the text
+        # return render(request, 'analyze.html', params)
+
+    if (newlineremover == "on"):
+        analyzed = ""
+        for char in djtext:
+            if char != "\n" and char!="\r":
+                analyzed = analyzed + char
+            else:
+                print("no")
+        print("pre", analyzed)
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+
+    if(removepunc != "on" and newlineremover!="on" and extraspaceremover!="on" and uppercase!="on" and lowercase!="on"):
+        return HttpResponse("please select any operation and try again")
+
+    return render(request, 'analyze.html', params)
+
+
+# def capfirst(request):
+#     return HttpResponse("capitalize first")
+#
+# def newlineremove(request):
+#     return HttpResponse("newline remove first")
+#
+#
+# def spaceremove(request):
+#     return HttpResponse("space remover back")
+#
+# def charcount(request):
+#     return HttpResponse("charcount ")
